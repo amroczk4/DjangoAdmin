5f5193d09315c372ff95a29512b40e6b770cf6d0
Matt Congreve <mcongrev@depaul.edu>
2022-10-10 18:48:53 -0500

    added html

--- app/foodrle/admin.py
+++ app/foodrle/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Dishes, Puzzle, UserStats
+
+admin.site.register(Dishes)
+admin.site.register(UserStats)
+admin.site.register(Puzzle)
