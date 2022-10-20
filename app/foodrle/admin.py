524b6d4c136d8f8e27f96af0a62c30b39427cb6d
amroczk4 <amroczk4@depaul.edu>
2022-10-20 17:07:58 -0500

    added all models to admin panel

--- app/foodrle/admin.py
+++ app/foodrle/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Dishes, Puzzle, UserStats
+from .models import Dishes, Puzzle, UserStats, MainIngredient, Taste, Country
 
 admin.site.register(Dishes)
 admin.site.register(UserStats)
 admin.site.register(Puzzle)
+admin.site.register(MainIngredient)
+admin.site.register(Taste)
+admin.site.register(Country)
