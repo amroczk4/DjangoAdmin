c72a258e04f48674da56ebd6bdc189a3039a73fc
amroczk4 <amroczk4@depaul.edu>
2022-10-20 17:09:37 -0500

    added taste hint

--- app/foodrle/game.py
+++ app/foodrle/game.py
@@ -1,17 +1,17 @@
 from random import randint
 from .models import Country, Dishes, Taste, MainIngredient
+from django.core.exceptions import ObjectDoesNotExist
+import os
+
 
 TOTAL_PUZZLES = 32
 user_stats = {
     1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 'games_played': 0
-    }
+}
 
 
-def main():
-    start_game()
-
-if __name__=="__main__":
-    main()
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 
 def update_user_stats(guess_cnt: int, isWin: bool):
@@ -19,15 +19,15 @@ def update_user_stats(guess_cnt: int, isWin: bool):
         cnt = user_stats.get(guess_cnt)
         if cnt is None:
             user_stats.update({guess_cnt: 1})
-        else: 
-            user_stats.update({guess_cnt: cnt+1})
+        else:
+            user_stats.update({guess_cnt: cnt + 1})
     played_games = user_stats.get('games_played')
     if played_games is None:
         user_stats.update({'games_played': 1})
     else:
-        user_stats.update({'games_played': played_games+1})
+        user_stats.update({'games_played': played_games + 1})
 
-    show_user_stats()    
+    show_user_stats()
     return
 
 
@@ -41,7 +41,7 @@ def print_welcome():
     print("\tYour game starts now")
 
 
-def get_dish(id: int) -> Dishes:
+def get_puzzle(id: int) -> Dishes:
     return Dishes.objects.get(pk=id)
 
 
@@ -55,14 +55,14 @@ def play_again() -> bool:
 
 
 def country_hint(answer_country: Country, guessed_country: Country):
-    country_hint = str(guessed_country.distance(answer_country)) + ' miles'
-    country_hint = country_hint + ' ' + guessed_country.direction(answer_country)
-    print('\tFrom a country',country_hint,f'of {guessed_country}')
+    cntry_hint = str(guessed_country.distance(answer_country)) + ' miles'
+    cntry_hint = cntry_hint + ' ' + guessed_country.direction(answer_country)
+    print('\tFrom a country', cntry_hint, f'of {guessed_country}')
 
 
 def main_ingredient_hint(answer: Dishes, guess_dish: Dishes):
     guess_main_ingr = guess_dish.main_ingredient
-    if answer.main_ingredient == guess_main_ingr:
+    if answer.main_ingredient.same_name_same_group(guess_main_ingr):
         print(f'\tCORRECT main ingredient: {answer.main_ingredient}')
     elif guess_main_ingr.food_group_eq(answer.main_ingredient):
         print(f'\tWRONG main ingredient: {guess_main_ingr.name}: CORRECT food group')
@@ -83,37 +83,61 @@ def calorie_hint(answer: Dishes, guess_dish: Dishes):
 
 
 def taste_hint(answer: Taste, guess_dish: Taste):
-    # stuff
-    print("\t<Placeholder for Taste Hint>")
+    taste_res = ''
+
+    if guess_dish.sweet:
+        if not answer.sweet:
+            taste_res = taste_res + 'NOT '
+        taste_res = taste_res + 'sweet; '
+    if guess_dish.salty:
+        if not answer.salty:
+            taste_res = taste_res + 'NOT '
+        taste_res = taste_res + 'salty; '
+    if guess_dish.sour:
+        if not answer.sour:
+            taste_res = taste_res + 'NOT '
+        taste_res = taste_res + 'sour; '
+    if guess_dish.bitter:
+        if not answer.bitter:
+            taste_res = taste_res + 'NOT '
+        taste_res = taste_res + 'bitter; '
+    if guess_dish.umami:
+        if not answer.umami:
+            taste_res = taste_res + 'NOT '
+        taste_res = taste_res + 'umami; '
+    print('\t'+taste_res)
 
 
 def start_game():
     print_welcome()
     # answer = get_dish_by_name('fries')
-    answer = get_dish(randint(1, TOTAL_PUZZLES))
+    answer = get_puzzle(randint(1, TOTAL_PUZZLES))
     guess_cnt = 0
     win = False
     while guess_cnt < 6:
-        guess_str = input(f'Enter Guess {guess_cnt+1}: ').lower()
+        guess_str = input(f'Enter Guess {guess_cnt + 1}: ').lower()
 
         # TODO: some manner of exception handling
-        guess_dish = get_dish_by_name(guess_str)
+        try:
+            guess_dish = get_dish_by_name(guess_str)
+        except Dishes.DoesNotExist:
+            print("Invalid answer: dish doesn\'t exist (yet)")
+            continue
 
         if guess_dish.name == answer.name:
             print("congratulations, you win!")
-            print(f'correct guess in {guess_cnt+1} tries')
+            print(f'correct guess in {guess_cnt + 1} tries')
             win = True
         else:
             print("[Hints] The correct answer is: ")
             country_hint(answer.country, guess_dish.country)
             calorie_hint(answer, guess_dish)
-            # main_ingredient_hint(answer.main_ingredient, guess_dish.main_ingredient)
-            taste_hint(answer.taste, guess_dish.taste) # TODO 
+            main_ingredient_hint(answer, guess_dish)
+            taste_hint(answer.taste, guess_dish.taste)  # TODO
             if guess_cnt >= 1:
                 reveal_main_ingredient(answer.main_ingredient)
-            if guess_cnt >= 4:
-                print("\tI am displaying a pixilated image of the answer")
-            
+            # if guess_cnt >= 4:
+            #     print("\tI am displaying a pixilated image of the answer")
 
         guess_cnt = guess_cnt + 1
         if guess_cnt == 6 or win:
@@ -122,7 +146,7 @@ def start_game():
             if play_again():
                 guess_cnt = 0
                 win = False
-                answer = get_dish(randint(1, TOTAL_PUZZLES))
+                answer = get_puzzle(randint(1, TOTAL_PUZZLES))
             else:
                 break
 
@@ -130,8 +154,5 @@ def start_game():
         print('Better luck next time!')
     else:
         print('Way to go!')
-    
-    show_user_stats()  
-    
 
-
+    show_user_stats()
