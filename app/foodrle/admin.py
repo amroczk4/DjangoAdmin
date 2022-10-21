8b94e0870fcd4650cea548486feda7ef9415310e
amroczk4 <amroczk4@depaul.edu>
2022-10-21 10:19:04 -0500

    Added Import/Export Feature to admin.py

--- app/foodrle/admin.py
+++ app/foodrle/admin.py
@@ -1,12 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
-
+from import_export.admin import ImportExportActionModelAdmin
 from .models import Dishes, Puzzle, UserStats, MainIngredient, Taste, Country
 
-admin.site.register(Dishes)
 admin.site.register(UserStats)
-admin.site.register(Puzzle)
-admin.site.register(MainIngredient)
-admin.site.register(Taste)
-admin.site.register(Country)
+
+
+@admin.register(Country)
+class CountryAdmin(ImportExportActionModelAdmin):
+    list_display = ("id", "name", "latitude", "longitude")
+    pass
+
+
+@admin.register(Taste)
+class TasteAdmin(ImportExportActionModelAdmin):
+    list_display = ("id", "sweet", "salty", "sour", "bitter", "umami")
+    pass
+
+
+@admin.register(MainIngredient)
+class MainIngredientAdmin(ImportExportActionModelAdmin):
+    list_display = ("id", "name", "food_group")
+    pass
+
+
+@admin.register(Puzzle)
+class PuzzleAdmin(ImportExportActionModelAdmin):
+    list_display = ("id", "last_used", "dish")
+    pass
+
+
+@admin.register(Dishes)
+class TasteAdmin(ImportExportActionModelAdmin):
+    list_display = ("id", "name", "country", "taste", "main_ingredient", "calories")
+    pass
+
