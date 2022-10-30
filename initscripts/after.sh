f1ee8c529d602be7098eecd7f5c14398d5b7fa2a
Matt Congreve <mcongrev@depaul.edu>
2022-10-30 17:53:14 -0500

    attempting to fix again

--- initscripts/after.sh
+++ initscripts/after.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose down && docker compose up -d --no-deps --build
+docker compose down -f ../docker-compose.yml && docker compose up -d --no-deps --build -f ../docker-compose.yml
