22ec3f60f0c8f928b05f2ce3262da1419ba1ad64
Matt Congreve <mcongrev@depaul.edu>
2022-10-30 17:38:27 -0500

    added dataset + import statement + fixed nginx.conf name

--- app/scripts/entrypoint.sh
+++ app/scripts/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
+python manage.py loaddata data/main.yaml
 python manage.py createsuperuser --no-input
 gunicorn -b :8000 app.wsgi:application
