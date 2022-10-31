90f531078757d16c9e27818b0887491083fb69ef
amroczk4 <96888069+amroczk4@users.noreply.github.com>
2022-10-31 18:42:20 -0500

    Changes to testpage

--- app/app/settings.py
+++ app/app/settings.py
@@ -53,7 +53,8 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=\'.\'',
+    # '--cover-package=foodrle'
+    '--cover-package=.',
     '--cover-html',
 ]
 
