d7d4dcff9faac8631509a08532ebec84085c5199
amroczk4 <amroczk4@depaul.edu>
2022-10-31 10:39:16 -0500

    Changed coverage args

--- app/app/settings.py
+++ app/app/settings.py
@@ -53,7 +53,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=foodrle',
+    '--cover-package=\'.\'',
     '--cover-html',
 ]
 
