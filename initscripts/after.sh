94ab5865ee3cb0c22e72b86f9c06363a5de79cb9
Matt Congreve <mcongrev@depaul.edu>
2022-10-30 21:06:42 -0500

    included forgotten files

--- initscripts/after.sh
+++ initscripts/after.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose -f /opt/foodrle/docker-compose.yml down && docker compose -f /opt/foodrle/docker-compose.yml up -d --no-deps --build
+docker compose -f /opt/foodrle/docker-compose.prod.yml down && docker compose -f /opt/foodrle/docker-compose.prod.yml up -d --no-deps --build
