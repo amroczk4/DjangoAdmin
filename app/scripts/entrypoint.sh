f182279ddc1b138978374527169f5b42032d1ade
Matt Congreve <mcongrev@depaul.edu>
2022-11-06 19:54:40 -0600

    moved fixture yml

--- app/scripts/entrypoint.sh
+++ app/scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata data/main.yaml
+python manage.py loaddata foodrle/fixtures/foodrle.yml
 python manage.py createsuperuser --no-input
 gunicorn -b :8000 app.wsgi:application
