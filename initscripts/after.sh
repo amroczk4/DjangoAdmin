0c367c93320cb3ed2ddc9b00361dc9773f2d31c0
Matt Congreve <mcongrev@depaul.edu>
2022-11-07 08:20:51 -0600

    clear up space before deployment

--- initscripts/after.sh
+++ initscripts/after.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker compose -f /opt/foodrle/docker-compose.prod.yml down && docker compose -f /opt/foodrle/docker-compose.prod.yml up -d --no-deps --build
+docker compose -f /opt/foodrle/docker-compose.prod.yml down && \
+docker system prune -a --volumes && \
+docker compose -f /opt/foodrle/docker-compose.prod.yml up -d --no-deps --build 
